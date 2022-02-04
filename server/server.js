9ca753ca0c5c59d6fece562a4b4e7aeeac94a827
Tony LaForgia <alforgia428@gmail.com>
2022-02-04 16:42:30 -0600

    added header and h1 title

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("server/public"));
+
+// PORT LISTEN
+app.listen(PORT, () => {
+  console.log("listening on port", PORT);
+});
