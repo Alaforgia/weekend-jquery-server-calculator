fab1227fc4b7f410339a9221c4e58e7308264ed1
Tony LaForgia <alforgia428@gmail.com>
2022-02-05 14:34:00 -0600

    Set up app.get/post server side

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("server/public"));
 
+//GET
+app.get("/calculator", (req, res) => {
+  console.log("Request at /calculator was made", req.body);
+});
+//POST
+app.post("/calculator", (req, res) => {
+  console.log("req.body from post", req.body);
+});
 // PORT LISTEN
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
