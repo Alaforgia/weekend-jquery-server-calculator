684d5fcf99e409e16c01999d25af6be88169f9af
Tony LaForgia <alforgia428@gmail.com>
2022-02-05 14:43:49 -0600

    Added onReady to client. Created on click

---
+++ server/public/scripts/client.js
@@ -0,0 +1,21 @@
+$(document).ready(onReady);
+
+function onReady() {
+    $("#plus").on('click', function)
+    $("#minus").on('click', function)
+    $("#times").on('click', function)
+    $("#divide").on('click', function)
+    $("#equals").on('click', function)
+    $("#clear-input").on('click', function)   
+}
+
+
+
+
+
+// GET FUNCTION
+// function get() {
+//     $.ajax({
+//       method: "GET",
+//       url: "/calculator",
+//     })
