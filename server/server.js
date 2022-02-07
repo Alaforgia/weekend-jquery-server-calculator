dc23ad56472845549d25564ed2aa4875e310c291
Tony LaForgia <alforgia428@gmail.com>
2022-02-07 16:50:16 -0600

    Changed my PORT

--- server/server.js
+++ server/server.js
@@ -3,11 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static("server/public"));
 
 const dataStore = [];
 
